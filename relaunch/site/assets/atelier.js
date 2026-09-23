4dc1a7fffb5dc4e49c87db59d91ac64ff7553aa9
Muammer Kizilaslan <Muammer.k@gmx.de>
2026-09-23 23:17:58 +0200

    Revert "Show documented KANTE design iteration in portfolio"
    This reverts commit 2885087c589c5c2c24ffc907c152ffcbdc320739.

--- relaunch/site/assets/atelier.js
+++
@@ -1,9 +0,0 @@
-(() => {
-  const peek = document.getElementById('atelierblick-kante');
-  if (!peek) return;
-  const revealLinkedPeek = () => {
-    if (location.hash === '#atelierblick-kante') peek.open = true;
-  };
-  revealLinkedPeek();
-  addEventListener('hashchange', revealLinkedPeek);
-})();
